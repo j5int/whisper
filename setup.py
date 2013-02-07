9187c540cefad6f82eb1b87c424254b682724712
Richard Graham <richardg@sjsoft.com>
2013-02-07 11:30:29 +0200

    Update version to 0.9.10-warden

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
   name='whisper',
-  version='0.9.10',
+  version='0.9.10-warden',
   url='https://launchpad.net/graphite',
   author='Chris Davis',
   author_email='chrismd@gmail.com',
